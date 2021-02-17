a5c5d7e65cb282a456a7995e8cad6b82c322ee95
girikusuma7 <girikusuma7@gmail.com>
2021-02-17 18:18:15 +0800

    perbaiki tampilan list dealer

--- resources/views/dealer/list.blade.php
+++ resources/views/dealer/list.blade.php
@@ -4,38 +4,40 @@
 
 @section('container')
 <div class="content-wrapper">
-  <div class="row">
-    <div class="col">
-      <div class="content-header">
-        <h1 class="m-0 text-dark">Daftar Dealer Motor</h1>
-        <hr>
-      </div>
-      <section class="content">
-        <div class="container">
-          <div class="row">
-            @foreach($getMerek as $item)
-            <div class="col">
-              <h4>{{ $item['merek'] }}</h4>
-            </div>
-            @endforeach
-          </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col">
+        <div class="content-header">
+          <h1 class="m-0 text-dark">Daftar Dealer Motor</h1>
+          <hr>
         </div>
-        <div class="container">
-          <div class="row">
-            @foreach($getMerek as $count)
-            <div class="col">
-              @foreach($getDealer as $item)
-                @if($item['merekDealer'] == $count['merek'])
-                <a href="{{ route('dealer.detail', [$provinsi, $kabupaten, $item['id']]) }}" class="text-decoration-none text-muted">
-                  <p>{{ $item['id'] }}</p>
-                </a>
-                @endif
+        <section class="content">
+          <div class="container-fluid">
+            <div class="row">
+              @foreach($getMerek as $item)
+              <div class="col">
+                <h4>{{ $item['merek'] }}</h4>
+              </div>
               @endforeach
             </div>
-            @endforeach
           </div>
-        </div>
-      </section>
+          <div class="container-fluid">
+            <div class="row">
+              @foreach($getMerek as $count)
+              <div class="col">
+                @foreach($getDealer as $item)
+                  @if($item['merekDealer'] == $count['merek'])
+                  <a href="{{ route('dealer.detail', [$provinsi, $kabupaten, $item['id']]) }}" class="text-decoration-none text-muted">
+                    <p>{{ $item['id'] }}</p>
+                  </a>
+                  @endif
+                @endforeach
+              </div>
+              @endforeach
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   </div>
 </div>
--- resources/views/servicecentre/list.blade.php
+++ resources/views/servicecentre/list.blade.php
@@ -4,39 +4,40 @@
 
 @section('container')
 <div class="content-wrapper">
-<div class="container-fluid">
-  <div class="row">
-    <div class="col">
-      <div class="content-header">
-        <h1 class="m-0 text-dark">Daftar Service Centre Motor</h1>
-        <hr>
-      </div>
-      <section class="content">
-      <div class="container">
-        <div class="row">
-          @foreach($getMerek as $item)
-            <div class="col">
-              <h4>{{ $item['merek'] }}</h4>
-            </div>
-          @endforeach
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col">
+        <div class="content-header">
+          <h1 class="m-0 text-dark">Daftar Service Centre Motor</h1>
+          <hr>
         </div>
-      </div>
-      <div class="container">
-        <div class="row">
-          @foreach($getMerek as $count)
-            <div class="col">
-              @foreach($getService as $item)
-                @if($item['merekService'] == $count['merek'])
-                  <a href="{{ route('service.detail', [$provinsi, $kabupaten, $item['id']]) }}" class="text-decoration-none text-muted">
-                    <p>{{ $item['id'] }}</p>
-                  </a>
-                @endif
+        <section class="content">
+          <div class="container-fluid">
+            <div class="row">
+              @foreach($getMerek as $item)
+                <div class="col">
+                  <h4>{{ $item['merek'] }}</h4>
+                </div>
               @endforeach
             </div>
-          @endforeach
-        </div>
+          </div>
+          <div class="container-fluid">
+            <div class="row">
+              @foreach($getMerek as $count)
+                <div class="col">
+                  @foreach($getService as $item)
+                    @if($item['merekService'] == $count['merek'])
+                      <a href="{{ route('service.detail', [$provinsi, $kabupaten, $item['id']]) }}" class="text-decoration-none text-muted">
+                        <p>{{ $item['id'] }}</p>
+                      </a>
+                    @endif
+                  @endforeach
+                </div>
+              @endforeach
+            </div>
+          </div>
+        </section>
       </div>
-      </section>
     </div>
   </div>
 </div>
