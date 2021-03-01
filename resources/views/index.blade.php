31590373b44907133b323446570e527736acd5a7
girikusuma7 <girikusuma7@gmail.com>
2021-03-01 20:45:24 +0800

    kuesioner

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,35 +11,35 @@
   </div>
   <div class="container">
     <div class="row">
-      <div class="col">
+      <div class="col-lg-4">
         <div class="card" style="width: 18rem;">
           <img class="card-img-top" src="images/browsingcard.jpg" alt="Card image cap" style="height : 12rem; width : 17.9rem;">
           <div class="card-body">
-            <a href="{{ url ('/browsing') }}"><h5 class="card-title btn btn-warning btn-lg btn-block">Browsing</h5></a>
+            <a href="{{ route('browsing') }}"><h5 class="card-title btn btn-warning btn-lg btn-block">Browsing</h5></a>
           </div>
         </div>
       </div>
-      <div class="col">
+      <div class="col-lg-4">
         <div class="card" style="width: 18rem;">
           <img class="card-img-top" src="images/searchingcard.jpg" alt="Card image cap" style="height : 12rem; width : 17.9rem;">
           <div class="card-body">
-            <a href="{{ url ('/searching') }}"><h5 class="card-title btn btn-success btn-lg btn-block">Searching</h5></a>
+            <a href="{{ route('searching.index') }}"><h5 class="card-title btn btn-success btn-lg btn-block">Searching</h5></a>
           </div>
         </div>
       </div>
-      <div class="col">
+      <div class="col-lg-4">
         <div class="card" style="width: 18rem;">
           <img class="card-img-top" src="images/rekomendasicard.jpg" alt="Card image cap" style="height : 12rem; width : 17.9rem;">
           <div class="card-body">
-            <a href="{{ url ('/rekomendasi') }}"><h5 class="card-title btn btn-dark btn-lg btn-block">Rekomendasi</h5></a>
+            <a href="{{ route('rekomendasi.index') }}"><h5 class="card-title btn btn-dark btn-lg btn-block">Rekomendasi</h5></a>
           </div>
         </div>
       </div>
-      <div class="col">
+      <div class="col-lg-4">
         <div class="card" style="width: 18rem;">
           <img class="card-img-top" src="images/questionnairecard.jpg" alt="Card image cap" style="height : 12rem; width : 17.9rem;">
           <div class="card-body">
-            <a href="{{ url ('/browsing') }}"><h5 class="card-title btn btn-info btn-lg btn-block">Questionnaire</h5></a>
+            <a href="https://forms.gle/QvFVHwEGkoupnfZ48" target="_blank"><h5 class="card-title btn btn-info btn-lg btn-block">Questionnaire</h5></a>
           </div>
         </div>
       </div>
