1d5de130862c8334faf2fcb63cff22ca9066df53
girikusuma7 <girikusuma7@gmail.com>
2021-03-15 17:30:32 +0800

    siap testing

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -134,6 +134,19 @@
             </a>
           </li>
         </ul>
+        <nav class="mt-2">
+          <p class="text-white">Tautan Lainnya</p>
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <li class="nav-item has-treeview menu-open">
+              <a href="https://krisna.balidigitalheritage.com" class="nav-link">
+                <i class="nav-icon fas fa-home"></i>
+                <p>
+                  Indekost
+                </p>
+              </a>
+            </li>
+          </ul>
+        </nav>
       </nav>
     </div>
   </aside>
